8734f907a203bc8d8c37b38b6e709eef7a1e3c5d
Dmitrii <SurikovDA@yandex.ru>
2023-10-19 16:41:51 +0300

    Основной сервис. 1 проверка

--- main-service/src/main/java/ru/practicum/category/service/CategoryServiceImpl.java
+++ main-service/src/main/java/ru/practicum/category/service/CategoryServiceImpl.java
@@ -1,13 +1,10 @@
 package ru.practicum.category.service;
 
-import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import com.sun.nio.sctp.IllegalReceiveException;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
