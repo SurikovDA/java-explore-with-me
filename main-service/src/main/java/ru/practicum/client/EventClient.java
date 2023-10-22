111e2d78a2a0edee225c5c21c07fb6ddf609a150
Dmitrii <SurikovDA@yandex.ru>
2023-10-22 20:27:29 +0300

    Основной сервис. 2 проверка

--- main-service/src/main/java/ru/practicum/client/EventClient.java
+++ main-service/src/main/java/ru/practicum/client/EventClient.java
@@ -2,68 +2,34 @@ package ru.practicum.client;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.practicum.ViewStatsClient;
-import ru.practicum.event.dto.ConfirmedEventDto;
 import ru.practicum.event.dto.EventShortDto;
 import ru.practicum.event.model.Event;
-import ru.practicum.event.model.RequestStatus;
-import ru.practicum.request.repository.ParticipationRequestRepository;
-import ru.practicum.dto.ViewStatsDto;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class EventClient {
-    private static final String START = "1970-01-01 00:00:00";
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-    private final ParticipationRequestRepository requestRepository;
-    private final ViewStatsClient viewStatsClient;
     private final EventClientService eventClientService;
 
-
     public static String formatTimeToString(LocalDateTime time) {
-        return time.format(FORMATTER);
+        return EventClientService.formatTimeToString(time);
     }
 
     public List<EventShortDto> makeEventShortDto(Collection<Event> events) {
-        Map<String, Long> viewStatsMap = toViewStats(events);
 
-        Map<Long, Long> confirmedRequests = getConfirmedRequests(events);
-
-        return eventClientService.makeEventShort(events, viewStatsMap, confirmedRequests);
+        return eventClientService.makeEventShort(events);
     }
 
     public Map<String, Long> toViewStats(Collection<Event> events) {
-        List<String> urisToSend = new ArrayList<>();
-        for (Event event : events) {
-            urisToSend.add(String.format("/events/%s", event.getId()));
-        }
 
-        List<ViewStatsDto> viewStats = viewStatsClient.getStats(
-                START,
-                LocalDateTime.now().format(FORMATTER),
-                urisToSend,
-                true
-        );
-
-        return viewStats.stream()
-                .collect(Collectors.toMap(ViewStatsDto::getUri, ViewStatsDto::getHits));
+        return eventClientService.toViewStats(events);
     }
 
     public Map<Long, Long> getConfirmedRequests(Collection<Event> events) {
-        List<Long> eventsIds = events.stream()
-                .map(Event::getId)
-                .collect(Collectors.toList());
-        List<ConfirmedEventDto> confirmedDtos =
-                requestRepository.countConfirmedRequests(eventsIds, RequestStatus.CONFIRMED);
-        return confirmedDtos.stream()
-                .collect(Collectors.toMap(ConfirmedEventDto::getEventId, ConfirmedEventDto::getCount));
+        return eventClientService.getConfirmedRequests(events);
     }
 }
--- main-service/src/main/java/ru/practicum/client/EventClientService.java
+++ main-service/src/main/java/ru/practicum/client/EventClientService.java
@@ -1,19 +1,39 @@
 package ru.practicum.client;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.practicum.ViewStatsClient;
+import ru.practicum.dto.ViewStatsDto;
+import ru.practicum.event.dto.ConfirmedEventDto;
 import ru.practicum.event.dto.EventShortDto;
 import ru.practicum.event.mapper.EventMapper;
 import ru.practicum.event.model.Event;
+import ru.practicum.event.model.RequestStatus;
+import ru.practicum.mapper.EndpointHitMapper;
+import ru.practicum.request.repository.ParticipationRequestRepository;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class EventClientService {
-    public List<EventShortDto> makeEventShort(Collection<Event> events, Map<String, Long> viewStatsMap,
-                                              Map<Long, Long> confirmedRequests) {
+    private static final String START = "1970-01-01 00:00:00";
+    private final ParticipationRequestRepository requestRepository;
+    private final ViewStatsClient viewStatsClient;
+
+    public static String formatTimeToString(LocalDateTime time) {
+        return time.format(EndpointHitMapper.DATE_TIME_FORMATTER);
+    }
+
+    public List<EventShortDto> makeEventShort(Collection<Event> events) {
+        Map<String, Long> viewStatsMap = toViewStats(events);
+
+        Map<Long, Long> confirmedRequests = getConfirmedRequests(events);
         List<EventShortDto> eventsDto = new ArrayList<>();
         for (Event event : events) {
             Long eventId = event.getId();
@@ -30,5 +50,32 @@ public class EventClientService {
             );
         }
         return eventsDto;
+    }
+
+    public Map<String, Long> toViewStats(Collection<Event> events) {
+        List<String> urisToSend = new ArrayList<>();
+        for (Event event : events) {
+            urisToSend.add(String.format("/events/%s", event.getId()));
+        }
+
+        List<ViewStatsDto> viewStats = viewStatsClient.getStats(
+                START,
+                LocalDateTime.now().format(EndpointHitMapper.DATE_TIME_FORMATTER),
+                urisToSend,
+                true
+        );
+
+        return viewStats.stream()
+                .collect(Collectors.toMap(ViewStatsDto::getUri, ViewStatsDto::getHits));
+    }
+
+    public Map<Long, Long> getConfirmedRequests(Collection<Event> events) {
+        List<Long> eventsIds = events.stream()
+                .map(Event::getId)
+                .collect(Collectors.toList());
+        List<ConfirmedEventDto> confirmedDtos =
+                requestRepository.countConfirmedRequests(eventsIds, RequestStatus.CONFIRMED);
+        return confirmedDtos.stream()
+                .collect(Collectors.toMap(ConfirmedEventDto::getEventId, ConfirmedEventDto::getCount));
     }
 }
